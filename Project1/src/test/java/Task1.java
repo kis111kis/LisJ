16e977a23c8d8bd062edf37f44149d016ec220fb
kis111kis <lisi.klors@mail.ru>
2022-09-24 15:50:44 +0300

    task1

--- Project1/src/test/java/Task1.java
+++ Project1/src/test/java/Task1.java
@@ -1,96 +1,86 @@
-public class Task1 {
-    public static int fact(int n) {
-        if (n == 1)
-            return n;
-        return n * fact(n - 1);
+public class Task1
+{
+    //посчитать факториал числа n
+    //0.5 балла - если посчитаете в цикле
+    //1 балл - если посчитаете рекурсией
+    public static int fact(int n){
+        return n;
     }
 
-    public static void table() {
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                System.out.println(i + "*" + j + "=" + i * j);
-            }
-            System.out.println();
-        }
+    //вывести таблицу умножения на экран - 1 балл
+    //подсказка - использовать двойной for
+    public static void table(){
+        //Ваше решение здесь
     }
 
-    public static int sum(int n) {
-        int sum = 0;
-        while (n > 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
+    //посчитать сумму цифр числа
+    //можно посчитать для трехзначного - 0.5 балла
+    //для любого числа - 1 балл
+    //подсказка - в случае для любого числа используйте while
+    public static int sum(int n){
+        //Ваше решение здесь
+        return n;
     }
 
+    //определить, является ли год високосным
+    //В високосном году - 366 дней, тогда как в обычном - 365.
+    //Високосным годом является каждый четвёртый год, за исключением столетий, которые не кратны 400.
+    //Так, годы 1700, 1800 и 1900 не являются високосными, так как они кратны 100 и не кратны 400.
+    //Годы 1600 и 2000 - високосные, так как они кратны 100 и кратны 400.
+    //Годы 2100, 2200 и 2300 - не високосные.
+    //за правильный ответ - 0.5 балла
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
-            return true;
-        return false;
+        //Ваше решение здесь
+        return true;
     }
 
-    public static void quicksort(int[] array, int l, int r){
-        int i = l, j = r;
-        int x = array[(l+r)/2];
-        while(i <= j){
-            while(array[i] < x) i++;
-            while(array[j] > x) j--;
-            if(i<=j){
-                int c = array[i];
-                array[i] = array[j];
-                array[j] = c;
-                i++; j--;
-            }
-        }
-        if(l < j)
-            quicksort(array, l, j);
-        if(i < r)
-            quicksort(array, i, r);
-    }
-
+    //здесь вам нужно будет использовать результат прошлой задачи
+    //и вывести, сколько дней в году
+    //правильный ответ - 0.5 балла
     public static int daysInYear(int year) {
-        if (isLeapYear(year)) {
-            System.out.println(366);
+        if (isLeapYear(year)){
+            //
         } else {
-            System.out.println(365);
+            //
         }
         return 0;
     }
 
-
-    public static int dayOfTheWeek(String n) {
-        String[] s = new String[] {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
-        for(int i = 0; i < 7; i++){
-            if(n.equals(s[i]))
-                return i+1;
-        }
+    //определить номер дня недели по строке
+    //например: Понедельник - 1
+    //правильный ответ - 1 балл
+    public static int dayOfTheWeek(String n){
+        //Ваше решение здесь
         return 0;
     }
 
-    public static void printArray(int[] array) {
-        System.out.print("[");
-        for (int i = 0; i < array.length; i++){
-            System.out.print(array[i]);
-            if(i != array.length - 1)
-                System.out.print(", ");
-        }
-        System.out.print("]\n");
+    //вывести массив на экран в виде: [1, 5, 3, 7, 10, 2, 5]
+    //правильное решение - 0.5 балла
+    public static void printArray(int[] array){
+        //Ваше решение здесь
     }
 
+    //вывести двойной массив на экран в виде:
+    // [1, 5, 3, 7, 10, 2, 5]
+    // [1, 5, 3, 7, 10, 2, 5]
+    // ...
+    //правильное решение - 0.5 балла
     public static void printArray(int[][] array){
-        for(int i = 0; i < array.length; i++){
-            System.out.print("[");
-            for(int j = 0; j < array[i].length; j++){
-                System.out.print(j);
-                if(j != array[i].length - 1)
-                    System.out.print(", ");
-            }
-            System.out.println("]");
-        }
+        //Ваше решение здесь
+    }
+
+    //отсортировать одномерный массив в порядке возрастания
+    //если не знаете, как сортировать, то подсказка -
+    //метод пузырька (один из самых простых для понимания)
+    //правильный ответ - 1 балл
+    public static int[] sort(int[] array){
+        //Ваше решение здесь
+        return array;
     }
 
 
-    public static void main(String[] args) {
+    //здесь можете тестировать свои решения
+    public static void main(String[] args){
         System.out.println("Факториал:");
         System.out.println(fact(5));
 
@@ -101,24 +91,17 @@ public class Task1 {
         System.out.println(sum(12345));
 
         System.out.println("Дней в году:");
-        System.out.println(daysInYear(2020));
+        System.out.println(daysInYear(2019));
 
         System.out.println("День недели:");
         System.out.println(dayOfTheWeek("Понедельник"));
 
         int[] array1D = {1,5,3,7,10,2,5};
         System.out.println("Вывод отсортированного массива:");
-        quicksort(array1D, 0, array1D.length-1);
-        printArray(array1D);
+        printArray(sort(array1D));
 
         System.out.println("Вывод двумерного массива:");
         int[][] array2D = {{1,5,3,7,10,2,5}, {1,5,3,7,10,2,5}};
         printArray(array2D);
     }
 }
-
-
-
-
-
-
